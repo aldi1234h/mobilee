8089dc99b204f18d1fe7e52f719369ac4321b539
aldi1234h <ardyansyahaldy83@gmail.com>
2021-02-09 11:53:05 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Tracking covid'),
+        ),
+        body: Center(
+          child: Text('Nama Panggilan Saya Adalah : Deco'),
+        ),
+      ),
+    );
+  }
+}
